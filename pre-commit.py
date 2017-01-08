60ed1b1224b2a8ba700fb7387a0186b0eb2d5570
madman61rus <rhjnhjtn3yjhe>
2017-01-08 16:50:52 +0300

    - Убрал тестировочный вывод из функции run_tests + Изменил опечатку в вызове функции run_tests - Убрал не нужные переменные

--- pre-commit.py
+++ pre-commit.py
@@ -7,22 +7,18 @@ def run_tests(file_with_tests):
                                stdout=subprocess.PIPE,
                                stderr=subprocess.STDOUT,
                                shell=True)
-    print(process)
     process.wait()
-    print(process.returncode)
+    return process.returncode
 
 
 if __name__ == '__main__' :
-
-    tests = ''
-    testing_file = ''
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-t", "--tests", required=True, help="path to a file with tests")
     parser.add_argument("-f", "--file", help="path to a testing file")
     args = parser.parse_args()
 
-    if args.tests == 0 :
+    if run_tests(args.tests) == 0 :
         print('Тесты прошли удачно')
     else:
         print('Тесты выявили ошибку')
